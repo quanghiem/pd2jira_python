7febf958cf8503415829b2f000ed6f4665eb01c9
Richard Nghiem <richard@nghiem.org>
2017-01-30 09:02:17 +0000

    remove exception stuff

--- pd2jira_python/alertToTicket.py
+++ pd2jira_python/alertToTicket.py
@@ -8,15 +8,11 @@ def lambda_handler(event, context):
 def main(event, is_lambda=True):
     messages = event['messages']
     for message in messages:
-        try:
-            message_data = message['data']
-            if PagerDuty.is_triggered_alert(message_data) and not Ticket.exists(message_data):  
-                ticket = Ticket(message_data, is_lambda)
-                ticket.create()
-                # TODO update_pager_duty_with_ticket_info()
-        except Exception as e:
-            print 'Error: {0}'.format(e)
-
+        message_data = message['data']
+        if PagerDuty.is_triggered_alert(message_data) and not Ticket.exists(message_data):
+            ticket = Ticket(message_data, is_lambda)
+            ticket.create()
+            # TODO update_pager_duty_with_ticket_info()
 
 if __name__ == '__main__':
     event = argv[1]
